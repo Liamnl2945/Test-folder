1dad9fc9a83b9d706feadbb751abd864a3f38505
CuzICanM8 <cuzican2017@outlook.com>
2024-01-17 18:31:18 -0700

    goon

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -2,47 +2,33 @@ package frc.robot.subsystems;
 
 import com.ctre.phoenix6.signals.NeutralModeValue;
 import com.ctre.phoenix6.hardware.TalonFX;
-import com.ctre.phoenix.motorcontrol.NeutralMode;
-import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
-
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.constants;
 
 public class Shooter extends SubsystemBase {
     
-    private final TalonFX ShooterLeft = new TalonFX(constants.Shooter.ShooterLeft);
-    private final TalonFX ShooterRigth = new TalonFX(constants.Shooter.ShooterRigth);
+    private final TalonFX ShooterLeft = new TalonFX(4);
+    private final TalonFX ShooterRight = new TalonFX(5);
     private final double speed = 0.4;
 
-public Shooter(){
-    ShooterLeft.TalonFXConfiguration();
-    ShooterRigth.configFactoryDefault();
+    public Shooter(){
+        ShooterLeft.setInverted(true);
+        ShooterRight.setNeutralMode(NeutralModeValue.Brake);
+    }
 
-    ShooterLeft.setInverted(true);
-    ShooterRigth.setInverted(false);
-    
-    ShooterLeft.setNeutralMode(NeutralMode.Brake);
-    ShooterRigth.setNeutralMode(NeutralMode.Brake);
-}
+    public void runShooter() {
+        ShooterLeft.set(-0.4);
+        ShooterRight.set(-0.4);
+    }
 
-public void runIntake() {
-    ShooterLeft.set(-0.4);
-    ShooterRigth.set(-0.4);
-}
+    public void reverseShooter() {
+        ShooterLeft.set(speed*2);
+        ShooterRight.set(speed*2);
+    }
 
-public void reverseIntake() {
-    ShooterLeft.set(speed*2);
-    ShooterRigth.set(speed*2);
-}
-public void slowReverseIntake(){
-    ShooterLeft.set(speed);
-    ShooterRigth.set(speed);
-}
-
-public void stopIntake(){
-    ShooterLeft.set(0);
-    ShooterRigth.set(0);
-}
+    public void stopIntake(){
+        ShooterLeft.set(0);
+        ShooterRight.set(0);
+    }
 
     
 }
